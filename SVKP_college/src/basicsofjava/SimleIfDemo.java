7ce717e0115296238998e2e8c285ddbabc8ffe31
kls99 <kls99@jhansi>
2024-11-04 05:57:40 +0530

    fourth commit

---
+++ SVKP_college/src/basicsofjava/SimleIfDemo.java
@@ -0,0 +1,18 @@
+package basicsofjava;
+
+public class SimleIfDemo {
+
+	public static void main(String[] args) {
+	int age=11;
+	if(age>18)
+	{
+		System.out.println("you are eligible");
+	}
+	else
+	{
+		System.out.println("you are not eligible");
+	}
+
+	}
+
+}
