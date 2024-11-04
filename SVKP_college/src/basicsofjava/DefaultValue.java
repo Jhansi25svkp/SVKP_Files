8f1ef435b251ce583178e8f19338ccd8596f6c12
kls99 <kls99@jhansi>
2024-11-04 05:51:51 +0530

    third commit

---
+++ SVKP_college/src/basicsofjava/DefaultValue.java
@@ -0,0 +1,16 @@
+
+class Demo
+{
+	String strvalues;
+	int a[];
+}
+
+public class DefaultValue {
+
+	public static void main(String[] args) {
+		Demo d1=new Demo();
+		System.out.println(d1.strvalues);
+		System.out.println(d1.a);
+	}
+
+}
---
+++ SVKP_college/src/basicsofjava/Demo.java
@@ -0,0 +1,10 @@
+package basicsofjava;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
