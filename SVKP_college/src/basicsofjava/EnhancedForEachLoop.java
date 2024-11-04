5d24e310ddd793d8ce2fae033f88e5b5c837f350
kls99 <kls99@jhansi>
2024-11-04 09:55:45 +0530

    fifth commit

---
+++ SVKP_college/src/basicsofjava/EnhancedForEachLoop.java
@@ -0,0 +1,13 @@
+package basicsofjava;
+
+public class EnhancedForEachLoop {
+	public static void main(String args[])
+	{
+	int num[]= {1,4,5,6,7,3,9};
+	for (int numbers:num)
+{
+		System.out.println(numbers);
+
+}
+}
+}
