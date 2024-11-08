f26a726c131585e5ca84e40599ec0628718b221f
kls99 <kls99@12753>
2024-11-08 13:07:48 +0530

    seventh commit

---
+++ SVKP_college/src/basicsofjava/Increment_Decrement_Demo.java
@@ -0,0 +1,18 @@
+package basicsofjava;
+
+public class Increment_Decrement_Demo {
+
+	public static void main(String[] args) {
+	 int a=10;
+	 
+	 //post increment
+	 System.out.println("a++" + a++); //print first then increment
+	 System.out.println("after post-increment a"+ a); //11
+	 
+	 //pre increment
+	 System.out.println("++a" +  ++a);  //increment first, then prints
+	 
+
+	}
+
+}
